14ef1696a6be3cf150e888e3d7e4ae0d51fa404b
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-09-30 17:03:20 +0200

    prepare for deploy in future

--- src/components/Measurements.tsx
+++ src/components/Measurements.tsx
@@ -12,33 +12,33 @@ const Measurements: React.FC = () => {
     const [measurements, setMeasurements] = useState<Measurement[]>([]);
   
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch('https://shark-app-7cyvy.ondigitalocean.app/get-data');
-          const data = await response.json();
-          setMeasurements(data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://shark-app-7cyvy.ondigitalocean.app/get-data');
+                const data = await response.json();
+                setMeasurements(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
   
-      fetchData();
+        fetchData();
     }, []);
   
     return (
-      <div>
-        <h1>Measurements</h1>
-        <ul>
-          {measurements.map(measurement => (
-            <li key={measurement.id}>
-              <p>Temperature: {measurement.temperature}°C</p>
-              <p>Humidity: {measurement.humidity}%</p>
-              <p>Timestamp: {new Date(measurement.timestamp).toLocaleString()}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div>
+            <h1>Measurements</h1>
+            <ul>
+            {measurements.map(measurement => (
+                <li key={measurement.id}>
+                    <p>Timestamp: {new Date(measurement.timestamp).toLocaleString()}</p>
+                    <p>Temperature: {measurement.temperature}°C</p>
+                    <p>Humidity: {measurement.humidity}%</p>
+                </li>
+            ))}
+            </ul>
+        </div>
     );
-  };
+};
   
-  export default Measurements;
+export default Measurements;
