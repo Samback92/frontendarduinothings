88f3c573ea72b5e546cc3f7b27750e6e013feb57
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-10-01 14:23:20 +0200

    Update Measurements.tsx
    implemented the filter function

--- src/components/Measurements.tsx
+++ src/components/Measurements.tsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { fetchData, deleteData } from '../services/api';
+import { isSameDay, getDaysAgo } from '../services/dateUtils';
 import MeasurementItem from './MeasurementItem';
+import FilterSelect from './FilterSelect';
+import Statistics from './Statistics';
 import { Measurement } from "../types";
 
 
 const Measurements: React.FC = () => {
     const [measurements, setMeasurements] = useState<Measurement[]>([]);
-  
+    const [filter, setFilter] = useState<string>("today");
+    const [filteredMeasurements, setFilteredMeasurements] = useState<Measurement[]>([]);
+    
+
     useEffect(() => {
         const loadMeasurements = async () => {
             try {
                 // const response = await fetch('http://localhost:8080/get-data');
                 const data = await fetchData<Measurement[]>('https://shark-app-7cyvy.ondigitalocean.app/get-data');
                 setMeasurements(data);
+                filterMeasurements(data, 'today');
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -25,10 +32,56 @@ const Measurements: React.FC = () => {
         try {
             // fetch(`http://localhost:8080/${dataId}`
             await deleteData(`https://shark-app-7cyvy.ondigitalocean.app/${dataId}`);
-            setMeasurements(measurements.filter(measurement => measurement.id !== dataId));
+            const updatedMeasurements = measurements.filter(measurement => measurement.id !== dataId);
+            setMeasurements(updatedMeasurements);
+            filterMeasurements(updatedMeasurements, filter);
         } catch (error) {
-            console.error('Error deleting data:', error);
+            console.error('Failed to delete task', error);
         }
+    };
+
+    const filterMeasurements = (data: Measurement[], filter: string) => {
+        const now = new Date();
+        let filteredData = data;
+
+        switch (filter) {
+            case 'all':
+                filteredData = data;
+                break;
+            case 'today':
+                filteredData = data.filter(measurement => isSameDay(new Date(measurement.timestamp), now));
+                break;
+            case 'yesterday':
+                filteredData = data.filter(measurement => isSameDay(new Date(measurement.timestamp), getDaysAgo(1)));
+                break;
+            case '2days':
+                filteredData = data.filter(measurement => isSameDay(new Date(measurement.timestamp), getDaysAgo(2)));
+                break;
+            case '3days':
+                filteredData = data.filter(measurement => isSameDay(new Date(measurement.timestamp), getDaysAgo(3)));
+                break;
+            case '4days':
+                filteredData = data.filter(measurement => isSameDay(new Date(measurement.timestamp), getDaysAgo(4)));
+                break;
+            case '5days':
+                filteredData = data.filter(measurement => isSameDay(new Date(measurement.timestamp), getDaysAgo(5)));
+                break;
+            case '6days':
+                filteredData = data.filter(measurement => isSameDay(new Date(measurement.timestamp), getDaysAgo(6)));
+                break;
+            case '1week':
+                filteredData = data.filter(measurement => isSameDay(new Date(measurement.timestamp), getDaysAgo(7)));
+                break;
+            default:
+                break;
+        }
+
+        setFilteredMeasurements(filteredData.reverse());
+    };
+
+    const handleFilterChange = (selectedFilter: string) => {
+        setFilter(selectedFilter);
+        filterMeasurements(measurements, selectedFilter);
     };
   
 
@@ -36,8 +89,12 @@ const Measurements: React.FC = () => {
         <>
             <div>
                 <h1>Measurements</h1>
+                <FilterSelect filter={filter} onFilterChange={handleFilterChange} />
                 <div>
-                    {measurements.map(measurement => (
+                    <Statistics measurements={filteredMeasurements} />
+                </div>
+                <div>
+                    {filteredMeasurements.map(measurement => (
                         <MeasurementItem key={measurement.id} measurement={measurement} onDelete={handleDelete} />
                     ))}
                 </div>
